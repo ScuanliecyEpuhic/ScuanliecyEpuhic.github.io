1c4a48a2cb14fc4ef02968f192115c09499adf05
ScuanliecyEpuhic <2414689874@qq.com>
2025-10-23 22:36:29 +0800

    test

--- site.config.ts
+++ site.config.ts
@@ -46,11 +46,11 @@ export default defineSiteConfig({
     options: {
       keys: ['title', 'tags', 'categories', 'excerpt', 'content'],
       /**
-       * @default 0.2
+       * @default 0.3
        * @see https://www.fusejs.io/api/options.html#threshold
        * 设置匹配阈值，越低越精确
        */
-      threshold: 0.2,
+      threshold: 0.3,
       /**
        * @default false
        * @see https://www.fusejs.io/api/options.html#ignoreLocation
